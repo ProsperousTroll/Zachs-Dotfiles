687eb5413580e6cf0f70f7321b8b6e1151725a3f
ProsperousTroll <ivekilled4dogs@gmail.com>
2025-06-20 06:01:20 -0700

    Cleanup aisle hypr, added random Beavis & Butthead quotes.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,15 @@
 if status is-interactive
+    # Set the fish greeting to nothing. 
     set -U fish_greeting ""
+
+    # Fastfetch
+    sleep 0.05 && fastfetch
+
+    # Print random Beavis & Butt-Head quote. (in italic, using ANSI color 92)
+    echo -e "\e[3;92m$(cat ~/Zachs-Dotfiles/fish/.config/fish/beavosquotes.txt | shuf -n 1)\e[23m"
+    # init starship
     starship init fish | source
+
+    # Cute in-joke alias for sudo
     alias sudude=sudo
-    # Commands to run in interactive sessions can go here
 end
