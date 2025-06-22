4a0552c1877366b2f7c96e4b2d9d62c5b55348be
ProsperousTroll <ivekilled4dogs@gmail.com>
2025-06-21 20:36:25 -0700

    Ported laptop changes over to desktop

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,15 @@
-# gets rid of annoying fish greeting prompt
-set fish_greeting ""
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Set the fish greeting to nothing. 
+    set -U fish_greeting ""
+
+    # Fastfetch
+    sleep 0.05 && fastfetch
+
+    # Print random Beavis & Butt-Head quote. (in italic, using ANSI color 92)
+    echo -e "\e[3;92m$(cat ~/Zachs-Dotfiles/fish/.config/fish/beavosquotes.txt | shuf -n 1)\e[23m"
+    # init starship
     starship init fish | source
+
+    # Cute in-joke alias for sudo
     alias sudude=sudo
 end
